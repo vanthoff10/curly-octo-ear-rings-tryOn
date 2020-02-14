0a41cd2c19faef94706e267b3eebf0c76fef9dc6
Atiksh Srivastava <ayushs125@gmail.com>
2020-02-14 22:55:18 +0530

    new test

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -24,7 +24,7 @@ export default class MediaComponent extends React.Component{
 			this.height=640
 			this.width=640	
 		} else{
-			this.height=window.innerHeight
+			this.height=window.innerWidth
 			this.width=window.innerWidth
 		}
 		
